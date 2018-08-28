97201a3ccbe687c10677555fc840774485bf8f91
AlexinaO <alexina.orsoni@gmail.com>
2018-08-28 15:38:59 +0200

    Ajout des paramètres de la classe Contexte

--- LocaMat.DAL/Data/Contexte.cs
+++ LocaMat.DAL/Data/Contexte.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace LocaMat.DAL.Data
 {
-    public class Contexte
+    public class Contexte : DbContext
     {
+        public DbSet<Agence> Agences { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<OffreProduit> OffresProduits { get; set; }
+        public DbSet<Produit> Produits { get; set; }
+        public DbSet<Clients> Clients { get; set; }
     }
 }
