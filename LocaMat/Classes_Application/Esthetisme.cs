a37d29616348feea82f374ca0e3a26688be5100b
AlexinaO <alexina.orsoni@gmail.com>
2018-08-28 16:14:32 +0200

    Création de la classe Esthetisme dans la couche application pour mise en forme du texte

---
+++ LocaMat/Classes_Application/Esthetisme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocaMat.Classes_Application
+{
+    public class Esthetisme
+    {
+        //Methode pour mettre le texte de la couleur voulue et centrer ou non le texte
+        public static void MiseEnFormeTexte(string message, ConsoleColor couleur = ConsoleColor.Gray, bool centre = false)
+        {
+            Console.ForegroundColor = couleur;
+            if (centre)
+            {
+                int nbEspaces = (Console.WindowWidth - message.Length) / 2;
+                Console.SetCursorPosition(nbEspaces, Console.CursorTop);
+            }
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
--- LocaMat/Program.cs
+++ LocaMat/Program.cs
@@ -10,6 +10,7 @@ namespace LocaMat
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
