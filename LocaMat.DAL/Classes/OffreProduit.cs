683b80d9ea362298d1a31ad067bbd1373f667154
AlexinaO <alexina.orsoni@gmail.com>
2018-08-28 15:09:35 +0200

    Ajout des paramètres à la classe OffreProduit

--- LocaMat.DAL/Classes/OffreProduit.cs
+++ LocaMat.DAL/Classes/OffreProduit.cs
@@ -8,5 +8,12 @@ namespace LocaMat.DAL.Classes
 {
     public class OffreProduit
     {
+        public int Id { get; set; }
+
+        public int IdProduit { get; set; }
+
+        public int IdAgence { get; set; }
+
+        public int Quantite { get; set; }
     }
 }
