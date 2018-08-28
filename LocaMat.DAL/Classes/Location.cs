7fd419e3560cdadb43cd11e9f73e8dcce1eb8dba
AlexinaO <alexina.orsoni@gmail.com>
2018-08-28 14:57:39 +0200

    Ajout des paramètres à la classe Location

--- LocaMat.DAL/Classes/Location.cs
+++ LocaMat.DAL/Classes/Location.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LocaMat.DAL.Classes
 {
     public class Location
     {
+        public int Id { get; set; }
+
+        public int IdProduit { get; set; }
+        [ForeignKey("IdProduit")]
+        public virtual Produit Produit { get; set; }
+
+        public int IdAgence { get; set; }
+        [ForeignKey("IdAgence")]
+        public virtual Agence Agence { get; set; }
+
+        public int IdClient { get; set; }
+        [ForeignKey("IdClient")]
+        public virtual Client Client { get; set; }
+
+        public DateTime Datedebut { get; set; }
+
+        public DateTime DateFin { get; set; }
+
+        public int Quantite { get; set; }
+
+        public Decimal TotalFacture { get; set; }
     }
 }
