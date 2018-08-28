c146903d7e181195fab8ca6e0b771328797a1dd6
AlexinaO <alexina.orsoni@gmail.com>
2018-08-28 15:12:41 +0200

    Ajout des FK à la classe OffreProduit

--- LocaMat.DAL/Classes/OffreProduit.cs
+++ LocaMat.DAL/Classes/OffreProduit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,12 @@ namespace LocaMat.DAL.Classes
         public int Id { get; set; }
 
         public int IdProduit { get; set; }
+        [ForeignKey ("IdProduit")]
+        public virtual Produit Produit { get; set; }
 
         public int IdAgence { get; set; }
+        [ForeignKey ("IdAgence")]
+        public virtual Agence Agence { get; set; }
 
         public int Quantite { get; set; }
     }
