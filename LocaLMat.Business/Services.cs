2ace3c20b13863809829379bca81ccce7446bab2
AlexinaO <alexina.orsoni@gmail.com>
2018-08-29 12:57:44 +0200

    Création de la méthode CreerAgence

--- LocaLMat.Business/Services.cs
+++ LocaLMat.Business/Services.cs
@@ -5,18 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using LocaMat.DAL.Data;
+using LocaMat.DAL.Classes;
 
 namespace LocaLMat.Business
 {
     public class Services
     {
-        public IEnumerable<TClasseChoisie> FaireListe <TClasseChoisie > ()
-            where TClasseChoisie:class
+        public IEnumerable<TClasseChoisie> FaireListe<TClasseChoisie>()
+            where TClasseChoisie : class
         {
             using (var contexte = new Contexte())
             {
                 return contexte.Set<TClasseChoisie>().ToList();
             }
+        }
+
+        /*public static void Creer(class TClasseChoisie)
+        {
+            if (TClasseChoisie == Agence | TClasseChoisie == Location | TClasseChoisie == OffreProduit)
+            {
+                Console.Write($"Créer une nouvelle {TClasseChoisie} :");
+            else Console.Write($"Créer un {TClasseChoisie} :");
+            }
+            var */
+        
+       public void CreerAgence(Agence agence)
+        {
+            using (var contexte = new Contexte())
+            {
+                contexte.Agences.Add(marque);
+                contexte.SaveChanges();
+            }
+        }
+
+
+                  
         }
     }
 }
