40b528cfd6d2f371d5fe754c86fd767ab5be051d
AlexinaO <alexina.orsoni@gmail.com>
2018-08-29 11:18:20 +0200

    Tentative de méthode FaireListe Générique

--- LocaLMat.Business/Services.cs
+++ LocaLMat.Business/Services.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using LocaMat.DAL.Data;
 
 namespace LocaLMat.Business
 {
-    class Services
+    public class Services
     {
+        public static void FaireListe(IEnumerable <ClasseChoisie>)
+        {
+            using (var contexte = new Contexte())
+            {
+                return contexte.ClasseChoisie
+                    .OrderBy(x => x.ClasseChoisie).ToList();
+            }
+        }
     }
 }
