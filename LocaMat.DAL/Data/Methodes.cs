4d75df9a95138670120e61ddee5d639c9c2e641d
AlexinaO <alexina.orsoni@gmail.com>
2018-08-29 12:53:01 +0200

    Création de la classe Methodes pour gérer les demandes

---
+++ LocaMat.DAL/Data/Methodes.cs
@@ -0,0 +1,25 @@
+﻿using LocaMat.DAL.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocaMat.DAL.Data
+{
+    public class Methodes
+    {
+        private static void CreerAgence()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Créer une nouvelle Agence");
+            Console.Write("Entrer la ville :");
+            var villeAgence = Console.ReadLine();
+            Console.Write("Entrer l'adresse :");
+            var adresseAgence = Console.ReadLine();
+            var agence = new Agence();
+            agence.Ville = villeAgence;
+            agence.Adresse = adresseAgence;
+        }
+    }
+}
