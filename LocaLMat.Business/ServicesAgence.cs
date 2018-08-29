0acfd7ea57d5ca3f486ef379aa994e604947b045
AlexinaO <alexina.orsoni@gmail.com>
2018-08-29 13:46:06 +0200

    Ajout de la classe ServicesAgence avec les méthodes pour accès à la base

---
+++ LocaLMat.Business/ServicesAgence.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using LocaMat.DAL.Data;
+using LocaMat.DAL.Classes;
+
+namespace LocaLMat.Business
+{
+    public class ServicesAgence
+    {
+        public void CreerAgence(Agence agence)
+        {
+            using (var contexte = new Contexte())
+            {
+                contexte.Agences.Add(agence);
+                contexte.SaveChanges();
+            }
+        }
+
+        public void ModifierAgence(Agence agence)
+        {
+            using (var contexte = new Contexte())
+            {
+                contexte.Agences.Attach(agence);
+                contexte.Entry(agence).State = EntityState.Modified;
+                contexte.SaveChanges();
+            }
+        }
+
+        public void SupprimerAgence(Agence agence)
+        {
+            using (var contexte = new Contexte())
+            {
+                contexte.Entry(agence).State = EntityState.Deleted;
+                contexte.SaveChanges();
+            }
+        }
+    }
+}
